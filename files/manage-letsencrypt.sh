af5662545563f51a7a77128691e7b17e44928c56
Tim Short <tim.short@cabernet.tech>
2023-04-14 14:05:47 +0100

    feature: support certbot --expand

--- files/manage-letsencrypt.sh
+++ files/manage-letsencrypt.sh
@@ -5,12 +5,14 @@ prog=$(basename $0)
 ss_action=''
 ss_contact=''
 ss_domain=''
+ss_expand=''
 ss_dry_run=''
 while getopts a:c:d:t arg; do
 	case $arg in
 		a) ss_action="${OPTARG}";;
 		c) ss_contact="${OPTARG}";;
 		d) ss_domain="${OPTARG}";;
+		e) ss_expand="--expand";;  # use if adding new domain to a cert
 		t) ss_dry_run='--dry-run';;
 		*) echo "ERROR :: $prog :: bad arg - bye"; exit 42
 	esac
@@ -31,7 +33,7 @@ if [[ "$ss_action" == "create" ]]; then
 		echo "ERROR :: $prog :: need a domain (-d) if creating a certificate"
 		exit 43
 	else
-		/usr/bin/certbot certonly -n --standalone --agree-tos -m "$ss_contact" -d "$ss_domain" ${ss_dry_run}
+		/usr/bin/certbot certonly -n --standalone --agree-tos -m "$ss_contact" -d "$ss_domain" ${ss_dry_run} ${ss_expand}
 		exit $?
 	fi
 elif [[ "$ss_action" == "renew" ]]; then

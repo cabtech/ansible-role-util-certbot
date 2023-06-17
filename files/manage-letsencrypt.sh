a0ed1c720a6b4b158394b6deee6638e212d19dd0
Tim Short <tim.short@cabernet.tech>
2023-06-17 13:50:29 +0100

    feature: add option to read domains from a file

--- files/manage-letsencrypt.sh
+++ files/manage-letsencrypt.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # MANAGED_BY_ANSIBLE
+#
+# config file format:
+#   one line per domain
+#   comment symbol is #
 
 prog=$(basename $0)
+
 ss_action=''
 ss_contact=''
 ss_domain=''
-ss_expand=''
 ss_dry_run=''
-while getopts a:c:d:t arg; do
+ss_expand=''
+ss_filename=''
+while getopts a:c:d:ef:t arg; do
 	case $arg in
 		a) ss_action="${OPTARG}";;
 		c) ss_contact="${OPTARG}";;
 		d) ss_domain="${OPTARG}";;
 		e) ss_expand="--expand";;  # use if adding new domain to a cert
+		f) ss_filename="${OPTARG}";;
 		t) ss_dry_run='--dry-run';;
 		*) echo "ERROR :: $prog :: bad arg - bye"; exit 42
 	esac
@@ -26,25 +33,37 @@ fi
 # --------------------------------
 
 if [[ "$ss_action" == "create" ]]; then
+	# if no domains on the command line, see if using a config file
+	if [[ -z "$ss_domain" ]]; then
+		if [[ -n "$ss_filename" ]]; then
+			# remove comments
+			# remove blank lines
+			# convert to CSV
+			# remove spaces
+			# remove trailing comma
+			ss_domain=$(cat $ss_filename | grep -v '#' | sed '/^ *$/d' | tr '\n' ',' | tr -d ' ' | sed 's/,$//')
+		fi
+	fi
+
 	if [[ -z "$ss_contact" ]]; then
 		echo "ERROR :: $prog :: need a contact (-c) if creating a certificate"
 		exit 43
 	elif [[ -z "$ss_domain" ]]; then
 		echo "ERROR :: $prog :: need a domain (-d) if creating a certificate"
-		exit 43
-	else
-		/usr/bin/certbot certonly -n --standalone --agree-tos -m "$ss_contact" -d "$ss_domain" ${ss_dry_run} ${ss_expand}
-		exit $?
+		exit 44
 	fi
+
+	/usr/bin/certbot certonly -n --standalone --agree-tos -m "$ss_contact" -d "$ss_domain" ${ss_dry_run} ${ss_expand}
+	exit $?
 elif [[ "$ss_action" == "renew" ]]; then
 	/usr/bin/certbot renew ${ss_dry_run}
 	exit $?
 else
 	echo "ERROR :: $prog :: unexpected action ($ss_action)"
-	exit 43
+	exit 45
 fi
 
 # --------------------------------
 # should not get here
 
-exit 44
+exit 46
